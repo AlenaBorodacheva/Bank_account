c94431587b5480ab71399814d2070fa84dd3422b
AlenaBorodacheva <alien_borodach@mail.ru>
2021-10-28 19:43:36 +0300

    create Building project

---
+++ Bank/Building(Lesson_4)/Building.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Building_Lesson_4_
+{
+    class Building
+    {      
+        private static int lastNumber = 0;
+        private int number;
+        private double high;
+        private int numberOfLevels;
+        private int numberOfAprtments;
+        private int numberOfEntrances;
+
+        internal Building()
+        {
+            CreateNewNumber();
+        }
+
+        private void CreateNewNumber()
+        {
+            lastNumber++;
+            number = lastNumber;
+        }
+
+        public void SetHigh(int high)
+        {
+            this.high = high;
+        }
+
+        public void SetNumberOfLevels(int numberOfLevels)
+        {
+            this.numberOfLevels = numberOfLevels;
+        }
+
+        public void SetNumberOfAprtments(int numberOfAprtments)
+        {
+            this.numberOfAprtments = numberOfAprtments;
+        }
+
+        public void SetNumberOfEntrances(int numberOfEntrances)
+        {
+            this.numberOfEntrances = numberOfEntrances;
+        }
+
+        public double GetNumber()
+        {
+            return number;
+        }
+
+        public double GetHigh()
+        {
+            return high;
+        }
+
+        public int GetNumberOfLevels()
+        {
+            return numberOfLevels;
+        }
+
+        public int GetNumberOfAprtments()
+        {
+            return numberOfAprtments;
+        }
+
+        public int GetNumberOfEntrances()
+        {
+            return numberOfEntrances;
+        }
+
+        public double GetHighLevel()
+        {
+            return high / numberOfLevels;
+        }
+
+        public int GetNumberOfApartmentOnEntrance()
+        {
+            return numberOfAprtments / numberOfEntrances;
+        }
+
+        public int GetNumberOfApartmentOnLevel()
+        {
+            return numberOfAprtments / numberOfEntrances / numberOfLevels;
+        }
+    }
+}
---
+++ Bank/Building(Lesson_4)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Building_Lesson_4_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building building = new Building();
+
+            building.SetHigh(15000);
+            building.SetNumberOfAprtments(100);
+            building.SetNumberOfEntrances(4);
+            building.SetNumberOfLevels(5);
+
+            double highLevel = building.GetHighLevel();
+            int numberOfApartmentOnEntrance = building.GetNumberOfApartmentOnEntrance();
+            int numberOfApartmentOnLevel = building.GetNumberOfApartmentOnLevel();
+        }
+    }
+}
